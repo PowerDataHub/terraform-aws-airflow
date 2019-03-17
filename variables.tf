6fd8b0ba6b707da474b4c7db7c1022e7a5617584
Eduardo Bizarro <edbizarro@gmail.com>
2019-03-16 23:48:37 -0300

    Hic Sunt Dracones

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  description = "AWS Region"
+  default     = "us-east-1"
+}
+
+variable "access_key" {
+  description = "AWS Access Key ID"
+}
+
+variable "secret_key" {
+  description = "AWS Secret Key"
+}
+
+variable "key_name" {
+  description = "SSH KeyPair"
+}
