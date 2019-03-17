c6e111c918b23a60535c05516e3e79f5134ae02f
Eduardo Bizarro <edbizarro@gmail.com>
2019-03-17 12:16:05 -0300

    data source vars

---
+++ ds.tf
@@ -0,0 +1,15 @@
+##############################################################
+# Data sources to get VPC, subnets and security group details
+##############################################################
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+data "aws_security_group" "default" {
+  vpc_id = "${data.aws_vpc.default.id}"
+  name   = "default"
+}
