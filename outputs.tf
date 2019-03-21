5aff11cd458745c5501db83b9b35345f35a54278
Eduardo Bizarro <edbizarro@gmail.com>
2019-03-21 02:14:35 -0300

    outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "airflow_instance_public_dns" {
-  value       = "${aws_instance.airflow_scheduler.public_dns}"
-  description = "Public DNS for the Airflow instance"
+output "airflow_webserver_public_dns" {
+  value       = "${aws_instance.airflow_webserver.public_dns}"
+  description = "Public DNS for the Airflow Webserver instance"
 }
 
-output "airflow_instance_public_ip" {
-  value       = "${aws_instance.airflow_scheduler.public_ip}"
-  description = "Public IP address for the Airflow instance"
+output "airflow_webserver_public_ip" {
+  value       = "${aws_instance.airflow_webserver.public_ip}"
+  description = "Public IP address for the Airflow Webserver instance"
 }
