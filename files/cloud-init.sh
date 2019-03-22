820b1616392d784652aad3fc4d2e14c653dfce96
Eduardo Bizarro <edbizarro@gmail.com>
2019-03-22 04:27:53 -0300

    provision script

--- files/cloud-init.sh
+++ files/cloud-init.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
-set -ex
+set -x
 
 function install_dependencyes() {
     sudo apt-get update -yqq && sudo apt-get upgrade -yqq
-	buildDeps=' \
-        freetds-dev \
-        libkrb5-dev \
-        libsasl2-dev \
-        libssl-dev \
-        libffi-dev \
-        libpq-dev \
-        git \
-    ' \
+	buildDeps='freetds-dev libkrb5-dev libsasl2-dev libssl-dev libffi-dev libpq-dev git libcurl4-openssl-dev' \
     && sudo apt-get install -yqq --no-install-recommends \
         $buildDeps \
-        freetds-bin \
-        build-essential \
-        default-libmysqlclient-dev \
+		bzip2 \
+		curl \
+		gcc \
+		git \
+		jq \
+		libcurl4-openssl-dev \
+		libssl-dev \
+		openssl \
+		postgresql-client \
+		python3 \
+		python3-pip \
+		python3-dev \
         apt-utils \
+        build-essential \
         curl \
-        rsync \
-        netcat \
+        freetds-bin \
         locales \
+        netcat \
+        rsync \
     && sed -i 's/^# en_US.UTF-8 UTF-8$/en_US.UTF-8 UTF-8/g' /etc/locale.gen \
     && locale-gen \
-    && update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 \
-    && useradd -ms /bin/bash -d $AIRFLOW_HOME airflow
+    && sudo update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 \
+    && sudo useradd -ms /bin/bash -d $AIRFLOW_HOME airflow
 }
 
 function install_python_and_python_packages() {
@@ -39,7 +42,10 @@ function install_python_and_python_packages() {
       --no-cache-dir --compile --ignore-installed \
       pycurl
 
+	pip3 install -qU setuptools --ignore-installed
+
     SLUGIFY_USES_TEXT_UNIDECODE=yes pip3 install -U \
+		Cython \
 		pytz \
 		pyOpenSSL \
 		ndg-httpsclient \
@@ -51,7 +57,6 @@ function install_python_and_python_packages() {
 		numpy \
 		scipy \
 		pandas \
-		setuptools \
 		apache-airflow[celery,postgres,s3,crypto,jdbc,google_auth,redis,slack,ssh]==1.10.2 \
 		celery[sqs] \
 		billiard==3.5.0.4 \
@@ -61,7 +66,7 @@ function install_python_and_python_packages() {
 
 function setup_airflow() {
     export AIRFLOW_HOME=/etc/airflow
-    mkdir -p /var/log/airflow
+    sudo mkdir -p /var/log/airflow
     AIRFLOW_HOME=/etc/airflow airflow initdb
 }
 
