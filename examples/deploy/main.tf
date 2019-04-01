4ac2d5aaf6346621cbf0d47d4eeb0b6be87170d4
Eduardo Bizarro <edbizarro@gmail.com>
2019-04-01 16:25:14 -0300

    examples

--- examples/deploy/main.tf
+++ examples/deploy/main.tf
@@ -2,19 +2,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#############################################################
-# Data sources to get VPC and default security group details
-#############################################################
+#################################
+# Data sources to get VPC details
+#################################
 data "aws_vpc" "default" {
   default = true
 }
 
 module "airflow-cluster" {
-  source            = "powerdatahub/airflow/aws"
-  cluster_name      = "airflow-example"
-  s3_bucket_name    = "airflow-example-logs"
-  db_password       = "123456789A*"                                  # Just for example purposes, for real projects you may want to create a terraform.tfvars file
-  fernet_key        = "HeY9Aivs7vADx5oy7SBKHfRJdj3fhpD_6IX2LnlDN74=" # Just for example purposes, for real projects you may want to create a terraform.tfvars file
-  aws_key_name      = "airflow-example-key"
-  load_example_dags = true
+  source             = "../../"
+  cluster_name       = "airflow-example"
+  s3_bucket_name     = "airflow-example-logs"
+  db_password        = "123456789A*"                                  # Just for example purposes, for real projects you may want to create a terraform.tfvars file
+  fernet_key         = "HeY9Aivs7vADx5oy7SBKHfRJdj3fhpD_6IX2LnlDN74=" # Just for example purposes, for real projects you may want to create a terraform.tfvars file
+  aws_key_name       = "airflow-example-key"
+  vpc_id             = "${data.aws_vpc.default.id}"
+  requirements_txt   = "./requirements.txt"
+  load_example_dags  = false
+  load_default_conns = false
+  rbac               = true
+  admin_username     = "edbizarro"                                    # Only if rbac is true
+  admin_password     = "321456987"                                    # Only if rbac is true
 }
