4384bc0b4bc21a9c098e0bbfb4faa69b9327ff8f
Eduardo Bizarro <edbizarro@gmail.com>
2019-07-14 14:59:13 -0300

    terraform fmt

--- examples/custom_env/main.tf
+++ examples/custom_env/main.tf
@@ -17,6 +17,6 @@ module "airflow_cluster" {
   fernet_key        = "HeY9Aivs7vADx5oy7SBKHfRJdj3fhpD_6IX2LnlDN74=" # Just for example purposes, for real projects you may want to create a terraform.tfvars file
   key_name          = "airflow-example-custom-env-key"
   vpc_id            = "${data.aws_vpc.default.id}"
-  custom_env        = "./.env"                                       # This envs will be appended and sourced by the airflow and OS system
+  custom_env        = "./.env" # This envs will be appended and sourced by the airflow and OS system
   load_example_dags = false
 }
--- examples/custom_requirements/main.tf
+++ examples/custom_requirements/main.tf
@@ -17,5 +17,5 @@ module "airflow_cluster" {
   fernet_key          = "HeY9Aivs7vADx5oy7SBKHfRJdj3fhpD_6IX2LnlDN74=" # Just for example purposes, for real projects you may want to create a terraform.tfvars file
   key_name            = "airflow-example-requirements-key"
   vpc_id              = "${data.aws_vpc.default.id}"
-  custom_requirements = "./requirements.txt"                           # Path to custom requirements.txt file
+  custom_requirements = "./requirements.txt" # Path to custom requirements.txt file
 }
