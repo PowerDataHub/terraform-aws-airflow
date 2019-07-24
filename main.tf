db5093a86435aa3f2fc40cf3a1f02885955dd53c
Floren Munteanu <fmunteanu@expedia.com>
2019-07-24 14:53:18 -0700

    Added proper version for source and missing tags

--- main.tf
+++ main.tf
@@ -11,38 +11,42 @@ terraform {
 # ---------------------------------------
 
 module "airflow_labels" {
-  source    = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=master"
+  source    = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.4.0"
   namespace = "${var.cluster_name}"
   stage     = "${var.cluster_stage}"
   name      = "airflow"
   delimiter = "-"
+  tags      = "${var.tags}"
 }
 
 module "airflow_labels_scheduler" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=master"
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.4.0"
   namespace  = "${var.cluster_name}"
   stage      = "${var.cluster_stage}"
   name       = "airflow"
   attributes = ["scheduler"]
   delimiter  = "-"
+  tags       = "${var.tags}"
 }
 
 module "airflow_labels_webserver" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=master"
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.4.0"
   namespace  = "${var.cluster_name}"
   stage      = "${var.cluster_stage}"
   name       = "airflow"
   attributes = ["webserver"]
   delimiter  = "-"
+  tags       = "${var.tags}"
 }
 
 module "airflow_labels_worker" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=master"
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.4.0"
   namespace  = "${var.cluster_name}"
   stage      = "${var.cluster_stage}"
   name       = "airflow"
   attributes = ["worker"]
   delimiter  = "-"
+  tags       = "${var.tags}"
 }
 
 resource "aws_key_pair" "auth" {
