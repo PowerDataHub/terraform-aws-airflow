e96582fefa35eb5b5c4b6e1dce4c991151d34ff8
Floren Munteanu <fmunteanu@expedia.com>
2019-07-30 16:15:26 -0400

    Moved labels to dedicated template

--- main.tf
+++ main.tf
@@ -10,45 +10,6 @@ terraform {
 # LABELS
 # ---------------------------------------
 
-module "airflow_labels" {
-  source    = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.4.0"
-  namespace = "${var.cluster_name}"
-  stage     = "${var.cluster_stage}"
-  name      = "airflow"
-  delimiter = "-"
-  tags      = "${var.tags}"
-}
-
-module "airflow_labels_scheduler" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.4.0"
-  namespace  = "${var.cluster_name}"
-  stage      = "${var.cluster_stage}"
-  name       = "airflow"
-  attributes = ["scheduler"]
-  delimiter  = "-"
-  tags       = "${var.tags}"
-}
-
-module "airflow_labels_webserver" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.4.0"
-  namespace  = "${var.cluster_name}"
-  stage      = "${var.cluster_stage}"
-  name       = "airflow"
-  attributes = ["webserver"]
-  delimiter  = "-"
-  tags       = "${var.tags}"
-}
-
-module "airflow_labels_worker" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.4.0"
-  namespace  = "${var.cluster_name}"
-  stage      = "${var.cluster_stage}"
-  name       = "airflow"
-  attributes = ["worker"]
-  delimiter  = "-"
-  tags       = "${var.tags}"
-}
-
 resource "aws_key_pair" "auth" {
   key_name   = "${var.key_name != "" ? var.key_name : module.airflow_labels.id}"
   public_key = "${file(var.public_key_path)}"
