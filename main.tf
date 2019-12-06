d4495af378262652e433b0d859f847fb68015bda
Eduardo Bizarro <edbizarro@gmail.com>
2019-12-06 15:11:12 -0300

    test

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@
 
 resource "aws_key_pair" "auth" {
   key_name   = var.key_name
-  public_key = coalesce(var.public_key, file(var.public_key_path))
+  public_key = var.public_key
 }
 
 # -------------------------------------------
